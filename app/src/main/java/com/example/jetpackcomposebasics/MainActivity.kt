ecf0d4d912074ff9427e8fc0b12cd0f67e8b738d
javier.suarez <javier.suarez@s-dos.es>
2023-02-22 19:23:53 +0100

    change light Theme and added animation to onclick

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.jetpackcomposebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
@@ -23,7 +26,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsTheme {
-                   MyApp()
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -31,16 +34,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")) {
+    modifier: Modifier = Modifier
+) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Column(modifier = modifier) {
-        for (name in names) {
-            Greeting(name = name)
+        if (shouldShowOnboarding) {
+            OnBoardingScreen(onBoardingContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
         }
+
     }
 
 
+}
+
+@Composable
+fun OnBoardingScreen(
+    onBoardingContinueClicked: () -> Unit
+  ) {
+
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the jungle")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick =  onBoardingContinueClicked ) {
+            Text(text = "Continue")
+        }
+
+    }
+
+}
+
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
+
+    LazyColumn() {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
 }
 
 @Composable
@@ -48,7 +85,12 @@ fun Greeting(name: String) {
 
     var expanded = remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 24.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 44.dp else 0.dp, animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -58,21 +100,30 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text("Hello")
-                Text(text = "$name!")
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.ExtraBold)
+                )
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value}) {
-                Text(if (expanded.value)  "Show more" else "Show less")
+            //TODO ADD IconButton
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show more" else "Show less")
             }
         }
     }
 }
 
+@Preview(showBackground = true,
+widthDp = 320,
+uiMode = UI_MODE_NIGHT_YES,
+name = "Dark Mode")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicsTheme {
-        MyApp()}
+        OnBoardingScreen(onBoardingContinueClicked = {})
+    }
 }
--- app/src/main/java/com/example/jetpackcomposebasics/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/ui/theme/Theme.kt
@@ -13,18 +13,10 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    surface = Blue,
+    onSurface = Chartreuse,
+    primary = LightBlue,
+    onPrimary = Navy
 )
 
 @Composable
